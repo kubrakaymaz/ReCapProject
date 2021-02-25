87406b28d874e89da627b7d29dd2637a334defff
Kübra KAYMAZ <kubra.kaymaz@std.izmirekonomi.edu.tr>
2021-02-25 21:03:00 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager:ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetCarsByBrandId()
+        {
+            return _carDal.Get(c => c.BrandId == BrandId);
+        }
+
+        public List<Car> GetCarsByColorId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    //public class InMemoryCarDal:ICarDal
+    //{
+    //    List<Car> _cars;
+    //    public InMemoryCarDal()
+    //    {
+    //        _cars = new List<Car> {
+    //            new Car{Id=1, BrandId=10, ColorId=2, ModelYear=1998, DailyPrice=20000,Description="-*----"},
+    //            new Car{Id=2, BrandId=20, ColorId=4, ModelYear=2005, DailyPrice=50000,Description="---*--"}
+    //            new Car{Id=3, BrandId=30, ColorId=6, ModelYear=2010, DailyPrice=100000,Description="--*---"},
+    //            new Car{Id=4, BrandId=40, ColorId=8, ModelYear=2020, DailyPrice=500000,Description="-**----"},
+    //        };
+    //    }
+    //    public void Add(Car car)
+    //    {
+    //        _cars.Add(car);
+    //    }
+
+    //    public void Update(Car car)
+    //    {
+    //        Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+    //        carToUpdate.Id = car.Id;
+    //        carToUpdate.BranId = car.BrandId;
+    //        carToUpdate.ColorId = car.ColorId;
+    //        carToUpdate.ModelYear = car.ModelYear;
+    //        carToUpdate.DailyPrice = car.DailyPrice;
+    //        carToUpdate.Description = car.Description;
+
+
+    //    }
+
+    //    public void Delete(Car car)
+    //    {
+    //        Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+
+    //        _cars.Remove(car);
+    //    }
+
+    //    public List<Car> GetAll(car)
+    //    {
+    //        return _cars;
+    //    }
+    //    public List<Car> GetByld(int Id)
+    //    {
+    //        return _cars.Where(c => c.Id == Id).ToList();
+
+    //    }
+
+    }
+
